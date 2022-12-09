5c52cf796d6ea0b242b6519b6419d6c974b6926d
Phaevourkhris <christopherfavz@gmail.com>
2022-12-09 13:20:48 -0800

    Print alphabet 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char y;
+
+	char z;
+
+	y = 'a';
+	z = 'A';
+	while
+		(y <= 'z') {
+			putchar(y);
+			y++;
+		}
+	while
+		(z <= 'Z') {
+			putchar(z);
+			z++;
+		}
+	putchar('\n');
+	return (0);
+}
