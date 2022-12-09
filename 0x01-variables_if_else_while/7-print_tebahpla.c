881761f7ead898ef89a1c03899933b5a36d4d146
Phaevourkhris <christopherfavz@gmail.com>
2022-12-09 13:54:54 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char y;
+
+	y = 'z';
+	while
+		(y >= 'a') {
+			putchar(y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}
