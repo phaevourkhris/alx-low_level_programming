f66f2c47cab3c8744aba4fb85cec12a3bd7f8a92
Phaevourkhris <christopherfavz@gmail.com>
2022-12-09 13:30:31 -0800

    print alphabet 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char y;
+
+	y = 'a';
+	while
+		(y <= 'z') {
+			if ((y != 'q' && y != 'e') && y <= 'z')
+				putchar(y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}
