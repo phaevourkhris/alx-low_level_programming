e4261e944824b2844823f590decd2de1f4d13f08
Phaevourkhris <christopherfavz@gmail.com>
2022-12-12 15:54:45 -0800

    correct Task 6 script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,12 @@
  */
 int main(void)
 {
+	long long int c;
+
 	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of long long int: %lu  byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of long long int: %lu  byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
