8a1e7575f3503c3a7602b6589212c0a33d6eb016
Phaevourkhris <christopherfavz@gmail.com>
2022-12-14 11:51:34 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,9 @@ void print_alphabet_x10(void)
 	int count;
 
 	for (count = 0; count < 10; count++)
-		for (alphabet = 'a'; alphabet <= z; alphabet++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
 		_putchar('\n');
+	}
 }
